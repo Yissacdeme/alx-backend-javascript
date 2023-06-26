45c7099a82739d1f56eb6dadbd80a3ac7a765779
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-06-27 12:46:41 +1300

    Create 6-string-interpolation.js

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,13 @@
+// Task 6
+// Original code
+// const name = "Alice";
+// const age = 25;
+
+// const greeting = "My name is " + name + " and I am " + age + " years old.";
+// console.log(greeting);
+
+const name = "Alice";
+const age = 25;
+
+const greeting = `My name is ${name} and I am ${age} years old.`;
+console.log(greeting);
