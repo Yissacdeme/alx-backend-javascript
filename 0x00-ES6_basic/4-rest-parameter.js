147759c86b571af89c45ff0b573c1f45b6e154f7
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-06-27 12:44:19 +1300

    Create 4-rest-parameter.js

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,21 @@
+// Task 4
+// Original code
+// const user = {
+//   name: 'John Doe',
+//   age: 30,
+//   country: 'USA',
+// };
+
+// const name = user.name;
+// const age = user.age;
+// const country = user.country;
+
+const user = {
+  name: 'John Doe',
+  age: 30,
+  country: 'USA',
+};
+
+const { name, age, country } = user;
+
+console.log(name, age, country);
