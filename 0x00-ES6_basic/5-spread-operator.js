e3c286a65de6a06ef527844fdcd256bd7dd992e1
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-06-27 12:45:28 +1300

    Create 5-spread-operator.js

---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,28 @@
+// Task 5
+// Original code
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const multiply = function(a, b) {
+//   return a * b;
+// };
+
+// const subtract = (a, b) => {
+//   return a - b;
+// };
+
+// const divide = (a, b) => a / b;
+
+const add = (a, b) => a + b;
+
+const multiply = (a, b) => a * b;
+
+const subtract = (a, b) => a - b;
+
+const divide = (a, b) => a / b;
+
+console.log(add(2, 3));
+console.log(multiply(4, 5));
+console.log(subtract(6, 7));
+console.log(divide(8, 2));
