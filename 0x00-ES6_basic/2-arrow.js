280e94467cbfda8d1cf44ed284f37a60f90d401b
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-06-27 12:41:21 +1300

    Create 2-arrow.js

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+// Task 2
+const name = 'Alice';
+const age = 25;
+
+// Original code
+// const message = 'My name is ' + name + ' and I am ' + age + ' years old.';
+const message = `My name is ${name} and I am ${age} years old.`;
+
+console.log(message);
