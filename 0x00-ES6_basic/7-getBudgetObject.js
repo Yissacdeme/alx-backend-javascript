4122b401549feb9c240a2e66ab0bf07866fc5a25
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-06-27 12:47:53 +1300

    Create 7-getBudgetObject.js

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,13 @@
+// Task 7
+// Original code
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const result = add(5, 3);
+// console.log(result);
+
+const add = (a, b) => a + b;
+
+const result = add(5, 3);
+console.log(result);
